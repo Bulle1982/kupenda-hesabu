7d9174371d02508c7b33b4078e39832cb44792bb
Bulle1982 <bullites114@hotmail.com>
2023-10-29 17:21:56 +0000

    Add docstrings to the functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,6 +71,10 @@ function runGame(gameType) {
     }
 }
 
+/**
+ * Checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
 
 function checkAnswer() {
 
@@ -89,6 +93,11 @@ function checkAnswer() {
     runGame(calculatedAnswer[1]);
 }
 
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
+
 function calculateCorrectAnswer() {
 
     operand1 = parseInt(document.getElementById("operand1").innerText);
@@ -104,12 +113,21 @@ function calculateCorrectAnswer() {
     }
 }
 
+
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+
 function incrementScore() {
 
     let oldScore = parseInt(document.getElementById("score").innerText);
     document.getElementById("score").innerText = ++oldScore;
 
 }
+
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 
 function incrementWrongAnswer() {
     let oldScore = parseInt(document.getElementById("incorrect").innerText);
